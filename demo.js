a0f2ad581bcdb50451a874c6b90beac46c08abe1
Коржев В.Н <REGION\vn.korzhev@r44-korzhev.region.tensor.ru>
2015-10-26 15:22:35 +0300

    format + raw data

--- demo.js
+++ demo.js
@@ -1,43 +1,44 @@
-var monitor = require('./'),
-   loopDone = false,
-   afterTicks = 0,
-   loopCount = 0;
+var ELMonitor = require('./').ELMonitor,
+    loopDone = false,
+    afterTicks = 0,
+    loopCount = 0,
+    monitor = new ELMonitor();
 
-setInterval(function dummy(){
-   // just to keep prcess running
+setInterval(function dummy() {
+    // just to keep prcess running
 }, 100);
 
 function loop() {
-   console.log('Starting heavy loop #%d', loopCount);
-   var s = +new Date();
-   while (true) {
-      if (+new Date() - s > 5000) {
-         console.log('Heavy loop #%d is done', loopCount);
-         loopCount++;
-         if (loopCount < 5) {
-            setImmediate(loop);
-         } else {
-            loopDone = true;
-         }
-         break;
-      }
-   }
+    console.log('Starting heavy loop #%d', loopCount);
+    var s = +new Date();
+    while (true) {
+        if (+new Date() - s > 5000) {
+            console.log('Heavy loop #%d is done', loopCount);
+            loopCount++;
+            if (loopCount < 5) {
+                setImmediate(loop);
+            } else {
+                loopDone = true;
+            }
+            break;
+        }
+    }
 
 }
 
-monitor.on('data', function(latencyData) {
-   console.log(JSON.stringify(latencyData, null, 2));
-   if (loopDone) {
-      afterTicks++;
-      if (afterTicks > 3) {
-         process.exit();
-      }
-   }
+monitor.on('data', function (latencyData) {
+    console.log(JSON.stringify(latencyData, null, 2));
+    if (loopDone) {
+        afterTicks++;
+        if (afterTicks > 3) {
+            process.exit();
+        }
+    }
 });
 
-monitor.resume();
+monitor.start(2000, 1);
 
 console.log('Waiting 10 sec and starting a 5 heavy loops');
-setTimeout(function(){
-   loop();
+setTimeout(function () {
+    loop();
 }, 10000);
